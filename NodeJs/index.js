fdc3eccd685ce20b4e439cf9dbdc91b567b9f41e
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-11-19 08:50:59 +0400

    test

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,13 +1,14 @@
 const http = require("http");
-const port = 5000;
+
+const port = 3000;
 const host = "127.0.0.1";
 
 const apiServer = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/plain");
-  response.end("First APi Server");
+  response.end("First Api Server");
 });
 
 apiServer.listen(port, host, () => {
-  console.log("sdsdfsdfsfsdf");
+  console.log(`http://${host}:${port}`);
 });
